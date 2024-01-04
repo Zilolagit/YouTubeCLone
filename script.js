c0e00921695b24ea123c5d90752e73d1b5d23587
Zilola Ixtiyorova <118010424+Zilolagit@users.noreply.github.com>
2024-01-04 19:03:17 +0500

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,228 @@
+let channels = [
+    {
+        id: 1,
+        name: "Easy Tutorials",
+        creator: "Zilola Ixtiyorova",
+        subscribers: 500000,
+        image: "https://w.forfun.com/fetch/b3/b3408c6e6ff5de76d852bc38518bdfc4.jpeg"
+    },
+    {
+        id: 2,
+        name: "Iroda.dev",
+        creator: "Tam Baker",
+        subscribers: 1000000000,
+        image: "https://w.forfun.com/fetch/8b/8b69e755f97a0501ff93e1edee9bffd6.jpeg"
+    },
+    {
+        id: 3,
+        name: "Kids Tutorials",
+        creator: "Malika Ixtiyorova",
+        subscribers: 500000,
+        image: "https://www.xtrafondos.com/wallpapers/lago-moraine-en-canada-2749.jpg"
+    },
+    {
+        id: 4,
+        name: "English with me",
+        creator: "Silmon Harder",
+        subscribers: 3000,
+        image: "https://i.pinimg.com/originals/0d/db/6d/0ddb6d65723da831a1dfc01f44600798.jpg"
+    },
+    {
+        id: 5,
+        name: "My moon",
+        creator: "Megan Ryan",
+        subscribers: 890000,
+        image: "https://w.forfun.com/fetch/b3/b3408c6e6ff5de76d852bc38518bdfc4.jpeg"
+    },
+]
+
+let videos = [
+    {
+        id: 1,
+        image: "https://a.l3n.co/i/8R4ErZ.jpg",
+        title: "Best YouTube Channel To Learn Web Development",
+        desc: "Channel that makes learning easy. Subscribe to my channel to watch more tutorials on web development",
+        created: "02/01/2024",
+        views: 150,
+        likes: 0,
+        dislikes: 0,
+        comments: [
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "01/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            }
+        ],
+        tags: ["HTML", "CSS", "Javascript"],
+        ownerName : "Zilola Ixtiyorova"
+    },
+    {
+        id: 2,
+        image: "https://w.forfun.com/fetch/95/95d31ed3228d6828a5a8f9f27cf242b0.jpeg",
+        title: "How to improve Ielts Read and Listening sections",
+        desc: "Channel that makes learning easy. Subscribe to my channel to watch more tutorials on web development",
+        created: "02/01/2024",
+        views: 150,
+        likes: 0,
+        dislikes: 0,
+        comments: [
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "01/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            }
+        ],
+        tags: ["HTML", "CSS", "Javascript"],
+        ownerName : "Zilola Ixtiyorova"
+    },
+    {
+        id: 2,
+        image: "https://w.forfun.com/fetch/95/95d31ed3228d6828a5a8f9f27cf242b0.jpeg",
+        title: "How to improve Ielts Read and Listening sections",
+        desc: "Channel that makes learning easy. Subscribe to my channel to watch more tutorials on web development",
+        created: "02/01/2024",
+        views: 350,
+        likes: 0,
+        dislikes: 0,
+        comments: [
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "01/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            }
+        ],
+        tags: ["HTML", "CSS", "Javascript"],
+        ownerName : "Zilola Ixtiyorova"
+    },
+    {
+        id: 2,
+        image: "https://w.forfun.com/fetch/95/95d31ed3228d6828a5a8f9f27cf242b0.jpeg",
+        title: "How to improve Ielts Read and Listening sections",
+        desc: "Channel that makes learning easy. Subscribe to my channel to watch more tutorials on web development",
+        created: "02/01/2024",
+        views: 150,
+        likes: 0,
+        dislikes: 0,
+        comments: [
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "01/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            }
+        ],
+        tags: ["HTML", "CSS", "Javascript"],
+        ownerName : "Zilola Ixtiyorova"
+    },
+    {
+        id: 2,
+        image: "https://w.forfun.com/fetch/95/95d31ed3228d6828a5a8f9f27cf242b0.jpeg",
+        title: "How to improve Ielts Read and Listening sections",
+        desc: "Channel that makes learning easy. Subscribe to my channel to watch more tutorials on web development",
+        created: "02/01/2024",
+        views: 150,
+        likes: 0,
+        dislikes: 0,
+        comments: [
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "02/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            },
+            {
+                name: "Jack Nicholson",
+                created: "01/01/2024",
+                image: "https://w.forfun.com/fetch/b4/b4061f05d1365648decb0cac791373a3.jpeg",
+            }
+        ],
+        tags: ["HTML", "CSS", "Javascript"],
+        ownerName : "Zilola Ixtiyorova"
+    },
+]
+
+
+let subscribers = ""
+let video = ""
+let subscribedList = document.querySelector(".subscribed-list")
 let menuIcon = document.querySelector(".menu-icon")
 let sidebar = document.querySelector(".sidebar")
 let container = document.querySelector(".container")
+let listContainer = document.querySelector(".list-container")
+
+for (let i = 0; i < channels.length; i++) {
+    subscribers += `<a href="javascript:void(0)"><img src="${channels[i].image}" alt="${channels[i].name}"><p>${channels[i].name}</p></a>`
+}
+
+subscribedList.innerHTML = subscribers
+
+
+function videoDetail(i) {
+    console.log(i);
+}
+
+for (let i = 0; i < videos.length; i++) {
+    video += `
+<div class="vid-list">
+    <a href="play-video.html" onclick="videoDetail(${videos[i].id})">
+        <img src="${videos[i].image}" alt="thumbnail" class="thumbnail">
+    </a>
+    <div class="flex-div">
+        <img src="${videos[i].image}" alt="jack">
+        <div class="vid-info">
+            <a href="play-video.html">Best channel to learn coding that help you to be a web developer</a>
+            <p>${videos[i].ownerName}</p>
+            <p>${videos[i].views.toLocaleString()} Views &bull; ${videos[i].created}</p>
+        </div>
+    </div>
+</div>`
+}
+
+listContainer.innerHTML = video
+
 menuIcon.onclick = () => {
     sidebar.classList.toggle("small-sidebar")
     container.classList.toggle("large-container")
