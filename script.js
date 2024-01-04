2db739fc58d822a858d2bbdcafb25c557b5e3ae9
Zilola Ixtiyorova <118010424+Zilolagit@users.noreply.github.com>
2024-01-04 07:53:03 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector(".menu-icon")
+let sidebar = document.querySelector(".sidebar")
+let container = document.querySelector(".container")
+menuIcon.onclick = () => {
+    sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
+}
